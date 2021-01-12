3747cc2642d81b31c606d87d5f0724d1bcc2563e
Shane Brunson <shanebrunson@Shane-Brunson-CHI-MBP-USBC.local>
2021-01-11 19:57:21 -0600

    updates after setting up new computer

--- brew_install.sh
+++ brew_install.sh
@@ -3,15 +3,14 @@ if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
-brew install yamllint
-brew install neofetch
-brew install fnm
-brew install awscli
-
-brew install firefox
-brew install spotify
-brew install iterm2
 brew install 1password
+brew install awscli
+brew install firefox
+brew install fnm
+brew install google-chrome
+brew install iterm2
 brew install spectacle
+brew install spotify
 brew install steam
-
+brew install visual-studio-code
+brew install yamllint
