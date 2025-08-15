e203d08f41168e5cf97bdf9b8ed6e7859930e38b
Shane Brunson <brunson.ws@gmail.com>
2025-08-14 21:38:15 -0500

    opam

--- stow/.config/fish/config.fish
+++ stow/.config/fish/config.fish
@@ -21,3 +21,12 @@ alias vimdiff="nvim -d"
 # Set neovim as default editor
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/shanebrunson/.opam/opam-init/init.fish' && source '/Users/shanebrunson/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
